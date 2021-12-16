60921223778edf8409b05e04bff17e619731e642
Mark Njuru <66862952+mariko971@users.noreply.github.com>
2021-12-16 20:25:59 +0400

    incorporated bootrap in the app for the home page carousel

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
-import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Home />
       </div>
     </Router>
   );
