1d4bf90a497a95024d579523c98fd3214952a537
Mark Njuru <66862952+mariko971@users.noreply.github.com>
2021-12-18 20:41:04 +0400

    added promo data into our DATA.js file

--- src/DATA.js
+++ src/DATA.js
@@ -1,158 +1,276 @@
-export const ads_Data = [
-  {
-    type: "byOne",
-    title: "Find the perfect gifts",
-    imageUrl:
-      "https://images-eu.ssl-images-amazon.com/images/G/39/UAE-hq/2021/img/Digital_Devices/AUCC/Devices_GW_DC_AE_brownie_EN._SY304_CB652093412_.jpg",
-    description: "Shop Now",
-  },
-  {
-    type: "byFour",
-    title: "Up to 30% off Home & Personal care",
-    ads: [
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/NDdkMGRmOWYt/NDdkMGRmOWYt-OWFhN2QwNjgt-w186._SY116_CB640155385_.jpg",
-        description: "Laundry & Detergent",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/NDdkMGRmOWYt/NDdkMGRmOWYt-M2VhZDhkODkt-w186._SY116_CB640155385_.jpg",
-        description: "Cleaning supplies",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/NDdkMGRmOWYt/NDdkMGRmOWYt-NGFmN2NkZGQt-w186._SY116_CB640155385_.jpg",
-        description: "Personal care",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/NDdkMGRmOWYt/NDdkMGRmOWYt-MTZjNTM0NTAt-w186._SY116_CB640155385_.jpg",
-        description: "Nutrition & Supplements",
-      },
-    ],
-    description: "See more",
-  },
-  {
-    type: "byFour",
-    title: "Up to 40% off Daily essentials",
-    ads: [
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/consumables_uae/EVENTS21/PNG21/png_homecare_186x116_ae-en._SY116_CB650372158_.jpg",
-        description: "Home care",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/consumables_uae/EVENTS21/PNG21/png_beauty_186x116_ae-en._SY116_CB650372158_.jpg",
-        description: "Beauty Essentials",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/consumables_uae/EVENTS21/PNG21/png_hpc_186x116_ae-en._SY116_CB650372158_.jpg",
-        description: "Health & Personal care",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/consumables_uae/EVENTS21/PNG21/png_baby_186x116_ae-en._SY116_CB650372158_.jpg",
-        description: "Baby Essentials",
-      },
-    ],
-    description: "See more offers",
-  },
-  {
-    type: "byFour",
-    title: "Save up to 20% on laptops",
-    ads: [
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Laptops/Feb_AllLaptops_1x._SY116_CB659039411_.jpg",
-        description: "All Laptops",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Laptops/Feb_2in1_1x._SY116_CB659039411_.jpg",
-        description: "2 in 1",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Laptops/Feb_Gaming_1x._SY116_CB659039411_.jpg",
-        description: "Gaming",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Laptops/Feb_Tablets_1x._SY116_CB659039411_.jpg",
-        description: "Tablets",
-      },
-    ],
-    description: "See more",
-  },
-  {
-    type: "byFour",
-    title: "Up to 20% off audio",
-    ads: [
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Audio/Feb_AllAudio_1x._SY116_CB659040113_.jpg",
-        description: "All Audio",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Audio/Feb_Earphones_1x._SY116_CB659040113_.jpg",
-        description: "Earphones",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Audio/Feb_Gaming_1x._SY116_CB659040113_.jpg",
-        description: "Gaming",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Audio/Feb_True_1x._SY116_CB659040113_.jpg",
-        description: "True Wireless",
-      },
-    ],
-    description: "See more",
-  },
-  {
-    type: "byFour",
-    title: "Gift ideas for the book lovers",
-    ads: [
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/Mjk3N2FlNDQt/Mjk3N2FlNDQt-Y2VhYjZkMWYt-w186._SY116_CB650210910_.jpg",
-        description: "Top rated Cookbooks",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/Mjk3N2FlNDQt/Mjk3N2FlNDQt-YjM1NzI0ZjEt-w186._SY116_CB650210910_.jpg",
-        description: "Travel books",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/Mjk3N2FlNDQt/Mjk3N2FlNDQt-OTY2NWM5OTIt-w186._SY116_CB650209749_.jpg",
-        description: "Box sets",
-      },
-      {
-        imageUrl:
-          "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/Mjk3N2FlNDQt/Mjk3N2FlNDQt-ZWZmZTk3NWIt-w186._SY116_CB650210910_.jpg",
-        description: "Romance",
-      },
-    ],
-    description: "See more",
-  },
-  {
-    type: "byOne",
-    title: "Rent your car here",
-    imageUrl:
-      "https://images-eu.ssl-images-amazon.com/images/G/39/AHS_UAE/2021/GW/CC-DC/web_CarRental_dc_1x_260x260._SY304_CB667298652_.jpg",
-    description: "Book Now",
-  },
-  {
-    type: "byOne",
-    title: "Save up to 20% on Game devices",
-    imageUrl:
-      "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/ZWI4YjY4MGQt/ZWI4YjY4MGQt-NjMwMjY5N2Ut-w379._SY304_CB641251285_.jpg",
-    description: "Shop Now",
-  },
-];
+export const ads_Data = {
+  promo: [
+    {
+      type: "byOne",
+      title: "Find the perfect gifts",
+      imageUrl:
+        "https://images-eu.ssl-images-amazon.com/images/G/39/UAE-hq/2021/img/Digital_Devices/AUCC/Devices_GW_DC_AE_brownie_EN._SY304_CB652093412_.jpg",
+      description: "Shop Now",
+    },
+    {
+      type: "byFour",
+      title: "Up to 30% off Home & Personal care",
+      ads: [
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/NDdkMGRmOWYt/NDdkMGRmOWYt-OWFhN2QwNjgt-w186._SY116_CB640155385_.jpg",
+          description: "Laundry & Detergent",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/NDdkMGRmOWYt/NDdkMGRmOWYt-M2VhZDhkODkt-w186._SY116_CB640155385_.jpg",
+          description: "Cleaning supplies",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/NDdkMGRmOWYt/NDdkMGRmOWYt-NGFmN2NkZGQt-w186._SY116_CB640155385_.jpg",
+          description: "Personal care",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/NDdkMGRmOWYt/NDdkMGRmOWYt-MTZjNTM0NTAt-w186._SY116_CB640155385_.jpg",
+          description: "Nutrition & Supplements",
+        },
+      ],
+      description: "See more",
+    },
+    {
+      type: "byFour",
+      title: "Up to 40% off Daily essentials",
+      ads: [
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/consumables_uae/EVENTS21/PNG21/png_homecare_186x116_ae-en._SY116_CB650372158_.jpg",
+          description: "Home care",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/consumables_uae/EVENTS21/PNG21/png_beauty_186x116_ae-en._SY116_CB650372158_.jpg",
+          description: "Beauty Essentials",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/consumables_uae/EVENTS21/PNG21/png_hpc_186x116_ae-en._SY116_CB650372158_.jpg",
+          description: "Health & Personal care",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/consumables_uae/EVENTS21/PNG21/png_baby_186x116_ae-en._SY116_CB650372158_.jpg",
+          description: "Baby Essentials",
+        },
+      ],
+      description: "See more offers",
+    },
+    {
+      type: "byFour",
+      title: "Save up to 20% on laptops",
+      ads: [
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Laptops/Feb_AllLaptops_1x._SY116_CB659039411_.jpg",
+          description: "All Laptops",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Laptops/Feb_2in1_1x._SY116_CB659039411_.jpg",
+          description: "2 in 1",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Laptops/Feb_Gaming_1x._SY116_CB659039411_.jpg",
+          description: "Gaming",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Laptops/Feb_Tablets_1x._SY116_CB659039411_.jpg",
+          description: "Tablets",
+        },
+      ],
+      description: "See more",
+    },
+    {
+      type: "byFour",
+      title: "Up to 20% off audio",
+      ads: [
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Audio/Feb_AllAudio_1x._SY116_CB659040113_.jpg",
+          description: "All Audio",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Audio/Feb_Earphones_1x._SY116_CB659040113_.jpg",
+          description: "Earphones",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Audio/Feb_Gaming_1x._SY116_CB659040113_.jpg",
+          description: "Gaming",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/electronics/store/kamja/WebQC/2021/Audio/Feb_True_1x._SY116_CB659040113_.jpg",
+          description: "True Wireless",
+        },
+      ],
+      description: "See more",
+    },
+    {
+      type: "byFour",
+      title: "Gift ideas for the book lovers",
+      ads: [
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/Mjk3N2FlNDQt/Mjk3N2FlNDQt-Y2VhYjZkMWYt-w186._SY116_CB650210910_.jpg",
+          description: "Top rated Cookbooks",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/Mjk3N2FlNDQt/Mjk3N2FlNDQt-YjM1NzI0ZjEt-w186._SY116_CB650210910_.jpg",
+          description: "Travel books",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/Mjk3N2FlNDQt/Mjk3N2FlNDQt-OTY2NWM5OTIt-w186._SY116_CB650209749_.jpg",
+          description: "Box sets",
+        },
+        {
+          imageUrl:
+            "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/Mjk3N2FlNDQt/Mjk3N2FlNDQt-ZWZmZTk3NWIt-w186._SY116_CB650210910_.jpg",
+          description: "Romance",
+        },
+      ],
+      description: "See more",
+    },
+    {
+      type: "byOne",
+      title: "Rent your car here",
+      imageUrl:
+        "https://images-eu.ssl-images-amazon.com/images/G/39/AHS_UAE/2021/GW/CC-DC/web_CarRental_dc_1x_260x260._SY304_CB667298652_.jpg",
+      description: "Book Now",
+    },
+    {
+      type: "byOne",
+      title: "Save up to 20% on Game devices",
+      imageUrl:
+        "https://images-eu.ssl-images-amazon.com/images/G/39/kindle/journeys/ZWI4YjY4MGQt/ZWI4YjY4MGQt-NjMwMjY5N2Ut-w379._SY304_CB641251285_.jpg",
+      description: "Shop Now",
+    },
+  ],
+  deals: [
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/31PPYVehGpL._AC_SY200_.jpg",
+      priceRange: { from: 12.13, to: 1260.02 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/41UUWCEbZ+L._AC_SY200_.jpg",
+      priceRange: { from: 51.17, to: 93.21 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/41zSiyJyhxS._AC_SY200_.jpg",
+      priceRange: { from: 12.16, to: 903.52 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/51XryVKbLgL._AC_SY200_.jpg",
+      priceRange: { from: 18.03, to: 84.12 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/41-oeBD43QL._AC_SY200_.jpg",
+      priceRange: { from: 28.53, to: 7540.02 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/31pPPvB+4XS._AC_SY200_.jpg",
+      priceRange: { from: 3.13, to: 459.62 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/21U8FqVYf3L._AC_SY200_.jpg",
+      priceRange: { from: 29.58, to: 679.09 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/31YfSQPUQKS._AC_SY200_.jpg",
+      priceRange: { from: 532.45, to: 711.02 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/4174yXmzcuL._AC_SY200_.jpg",
+      priceRange: { from: 141.32, to: 333.42 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/31-F4phgClL._AC_SY200_.jpg",
+      priceRange: { from: 502.13, to: 1070.02 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/310P2f1p07L._AC_SY200_.jpg",
+      priceRange: { from: 137.73, to: 506.92 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/41KfT5KDAwS._AC_SY200_.jpg",
+      priceRange: { from: 69.0, to: 339.0 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/31XrIUF7I0L._AC_SY200_.jpg",
+      priceRange: { from: 143.0, to: 212.0 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/41Q8F3hJFQL._AC_SY200_.jpg",
+      priceRange: { from: 21.75, to: 12489.02 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/31NRAjl5JeL._AC_SY200_.jpg",
+      priceRange: { from: 238.0, to: null },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/3193tNFJW2L._AC_SY200_.jpg",
+      priceRange: { from: 12.0, to: 604.61 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/41Vio5oe0bS._AC_SY200_.jpg",
+      priceRange: { from: 6.96, to: 261.41 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/316O7CNGq9L._AC_SY200_.jpg",
+      priceRange: { from: 8.66, to: 620.93 },
+      currency: "AED",
+    },
+    {
+      imageUrl:
+        "https://m.media-amazon.com/images/I/51WroLx4gyL._AC_SY200_.jpg",
+      priceRange: { from: 17.5, to: 209.3 },
+      currency: "AED",
+    },
+  ],
+};
